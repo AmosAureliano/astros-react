1482b02ad32ddb2d46030d06ed6854931c313f3e
Amós Aureliano <amos.aureliano@gmail.com>
2021-08-18 18:14:24 -0300

    Correcao Request

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 import api from '../../services/api';
 import Asteroid from '../../components/Asteroid';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 function Home(){
 
@@ -11,40 +11,21 @@ function Home(){
     const listOfElements = [];
     const apiKey = process.env.REACT_APP_NASA_API_KEY;
 
-    //Teste Retorno API
-    useEffect(() => {
-        api.get(`feed?start_date=2021-08-16&end_date=2021-08-17&api_key=${apiKey}`).then(({data}) => { 
-            setAsteroids(data.near_earth_objects);
-            
-        })
-    },[]);
-    
-    for(var item in asteroids){
-        for(var element in asteroids[item]){
-            listOfElements.push(asteroids[item][element])
-        }
-    }
-
-    //Teste retorno Por data dinâmica
-
-    /*
     function iterator(){
         for(var item in asteroids){
             for(var element in asteroids[item]){
                 listOfElements.push(asteroids[item][element])
             }
         }
-    }*/
+    }
     
-    
-    
-    /*
     function requestByDate(startDate, endDate){
         api.get(`feed?start_date=${startDate}&end_date=${endDate}&api_key=${apiKey}`).then(({data}) => { 
             setAsteroids(data.near_earth_objects);
         })
         iterator();
-    }*/
+        console.log(asteroids)
+    }
 
     return(   
         <div className="container">
@@ -52,24 +33,25 @@ function Home(){
                 <h1 className="title">Astro<span>'</span>s</h1>
                 <p className="description">Saiba quais asteroides passaram próximos da terra em um intervalo de tempo!</p>
                 
-                <form className="form">
+                <form className="form" >
                     <div className="input">
                         <label  className="">Data Início</label>
                         {/**Implementar Callback*/}
                         <input type="date" value={startDate} id="startDate" onChange={(event)=>{ 
                             setStartDate(event.target.value); 
-                            console.log(startDate)
                         }}/>
                     </div>
                     <div className="input">
                         <label className="">Data Final</label>
                         {/**Implementar Callback no value*/}
-                        <input type="date"value={endDate} id="endDate"/>
+                        <input type="date"value={endDate} id="endDate" onChange={(event)=>{ 
+                            setEndDate(event.target.value); 
+                        }}/>
                     </div>
-                    <button type="submit" className="btn" >Buscar</button>
+                    <button type="submit" className="btn" type="button" onClick={() => {requestByDate(startDate, endDate)}}>Buscar</button>
                 </form>
                 
-                <div>
+                <div className="list">
                     {listOfElements.map((asteroid) => (     
                         <Asteroid 
                             key={asteroid.id}
@@ -78,8 +60,7 @@ function Home(){
                             approachDate={asteroid.close_approach_data[0].close_approach_date_full}
                             danger={asteroid.danger ? 'sim' : 'não'}
                         />
-                    ))
-                    }
+                    ))}
                 </div>
             </section>
         </div>
